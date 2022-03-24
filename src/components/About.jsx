d9ee1ab7eb6ea00762c83d0fcaf366c64e2413dd
Argie Barcena <barcenaargie@gmail.com>
2022-03-24 22:53:36 +0800

    Add layout for home and contact page with responsiveness

---
+++ src/components/About.jsx
@@ -0,0 +1,166 @@
+import React, { useEffect } from 'react';
+
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+
+// Framer Motion
+import { useInView } from 'react-intersection-observer';
+import { motion, useAnimation } from 'framer-motion';
+
+import {
+    Accordion,
+    AccordionItem,
+    AccordionItemHeading,
+    AccordionItemButton,
+    AccordionItemPanel,
+} from 'react-accessible-accordion';
+
+import 'react-accessible-accordion/dist/fancy-example.css';
+
+// SVG
+import mission from '../assets/svg/mission.svg';
+import vision from '../assets/svg/vision.svg';
+
+const About = () => {
+    const { ref, inView } = useInView({
+        threshold: 0.25
+    });
+
+    const animationLeft = useAnimation();
+    const animationRight = useAnimation();
+    const fade = useAnimation();
+
+    useEffect(() => {
+        if (inView) {
+            fade.start({
+                opacity: 1
+            });
+            animationLeft.start({
+                x: 0,
+                transition: {
+                    type: 'spring', duration: 1, bounce: 0.3
+                }
+            });
+            animationRight.start({
+                x: 0,
+                transition: {
+                    type: 'spring', duration: 1, bounce: 0.3
+                }
+            });
+        }
+
+        if (!inView) {
+            fade.start({
+                opacity: 0
+            });
+            animationLeft.start({
+                x: '-100px',
+            });
+            animationRight.start({
+                x: '100px'
+            });
+        }
+    }, [inView]);
+
+    return (
+        <div className="about__container" id="about">
+            <div className="about__container--section h-auto p-4 flex flex-col items-center justify-center">
+                <div className="about__container--header w-11/12">
+                    <h2 className="text-5xl font-bold lg:text-5xl md:text-5xl sm:text-4xl custom:text-2xl">
+                        We Are
+                        <br />
+                        <span className="text-green-800">JNCE MEDICAL & </span>
+                        <br />
+                        <span className="text-green-800">DIAGNOSTIC CLINIC</span>
+                    </h2>
+                </div><br /><br />
+                <div className="about__container--body overflow-hidden w-11/12 flex flex-col justify-center lg:block md:hidden sm:hidden custom:hidden">
+                    <div className="about__container--quality--policy">
+                        <h1 className="text-3xl text-center font-bold">QUALITY POLICY</h1>
+                    </div><br />
+                    <div>
+                        <p className="text-2xl indent-8">JNCE Medical and Diagnostic Clinic shall strive to satisfy our clients by providing them quality medical service at reasonable cost. We shall continually strive to maintaiin excellence by maintaining a high qualified, efficient and competent medical staffs.</p>
+                    </div><br />
+                    <div className="about__container--quality--objective">
+                        <h1 className="text-3xl text-center font-bold">QUALITY OBJECTIVES</h1>
+                    </div><br />
+                    <div>
+                        <p className="text-2xl indent-8">To satisfy clients thru prompt delivery of medical services. To delivery PEME results within 24 hours after completion of required PEME package. To comply 100% to legal regulatory requirement for medical clinics. To control repartriation rate not to exceed 0.1% annually.</p>
+                    </div><br /><br />
+
+
+                    <div className="about__container--mission grid grid-cols-2 place-items-center">
+                        <div ref={ref} className="about__mission">
+                            <motion.h1 animate={animationLeft} className="text-4xl text-center font-bold">Mission</motion.h1>
+                            <motion.p animate={fade} className="text-xl indent-8 mt-4 text-center italic leading-10">"To render quality medical service to satisfy our client's needs.<br /> To maintain excellence and professionalism in the delivery of medical service<br /> in accordance with the national and international standards."</motion.p>
+                        </div>
+                        <div className="about_mission--img">
+                            <LazyLoadImage
+                                alt="mission svg"
+                                src={mission}
+                            />
+                        </div><br />
+                    </div>
+                    <div className="about__container--vision grid grid-cols-2 place-items-center">
+                        <div className="about_vision--img">
+                            <img src={vision} alt="vision svg" />
+                        </div>
+                        <div ref={ref} className="about__vision">
+                            <motion.h1 animate={animationRight} className="text-4xl text-center font-bold">Vision</motion.h1>
+                            <motion.p animate={fade} className="text-xl indent-8 mt-4 text-center italic leading-10">"JNCE Medical and Diagnostic Clinic envisions<br /> to become a high-standard provider of pre-employment<br /> medical services for local and overseas workers."</motion.p>
+                        </div>
+                    </div>
+                </div> <br />
+                <Accordion allowZeroExpanded className="w-full lg:hidden md:block sm:block custom:block">
+                    <AccordionItem>
+                        <AccordionItemHeading>
+                            <AccordionItemButton>
+                                QUALITY POLICY
+                            </AccordionItemButton>
+                        </AccordionItemHeading>
+                        <AccordionItemPanel>
+                            <p className="indent-8">JNCE Medical and Diagnostic Clinic shall strive to satisfy our clients by providing them quality medical service at reasonable cost. We shall continually strive to maintaiin excellence by maintaining a high qualified, efficient and competent medical staffs.</p>
+                        </AccordionItemPanel>
+                    </AccordionItem>
+                    <AccordionItem>
+                        <AccordionItemHeading>
+                            <AccordionItemButton>
+                                QUALITY OBJECTIVES
+                            </AccordionItemButton>
+                        </AccordionItemHeading>
+                        <AccordionItemPanel>
+                            <p className="indent-8">To satisfy clients thru prompt delivery of medical services. To delivery PEME results within 24 hours after completion of required PEME package. To comply 100% to legal regulatory requirement for medical clinics. To control repartriation rate not to exceed 0.1% annually.</p>
+                        </AccordionItemPanel>
+                    </AccordionItem>
+                    <AccordionItem>
+                        <AccordionItemHeading>
+                            <AccordionItemButton>
+                                MISSION
+                            </AccordionItemButton>
+                        </AccordionItemHeading>
+                        <AccordionItemPanel>
+                            <LazyLoadImage
+                                alt="mission svg"
+                                src={mission}
+                            />
+                            <p className="indent-8">To render quality medical service to satisfy our client's needs. To maintain excellence and professionalism in the delivery of medical service in accordance with the national and international standards.</p>
+                        </AccordionItemPanel>
+                    </AccordionItem>
+                    <AccordionItem>
+                        <AccordionItemHeading>
+                            <AccordionItemButton>
+                                VISION
+                            </AccordionItemButton>
+                        </AccordionItemHeading>
+                        <AccordionItemPanel>
+                            <img src={vision} alt="vision svg" />
+                            <p className="indent-8">JNCE Medical and Diagnostic Clinic envisions to become a high-standard provider of pre-employment medical services for local and overseas workers.</p>
+                        </AccordionItemPanel>
+                    </AccordionItem>
+                </Accordion>
+
+            </div>
+        </div>
+    )
+}
+
+export default About
---
+++ src/components/Home.jsx
@@ -0,0 +1,161 @@
+import React, { useEffect } from 'react';
+
+import ImageGallery from 'react-image-gallery';
+
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+
+// Framer Motion
+import { useInView } from 'react-intersection-observer';
+import { motion, useAnimation } from 'framer-motion';
+
+// SVG
+import accurate from '../assets/svg/accurate.svg';
+import cheap from '../assets/svg/cheap.svg';
+import quality from '../assets/svg/quality.svg';
+import delivery from '../assets/svg/delivery.svg';
+
+const images = [
+  {
+    original: 'https://images.unsplash.com/photo-1589279003513-467d320f47eb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+  },
+  {
+    original: 'https://images.unsplash.com/photo-1612277795421-9bc7706a4a34?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+  },
+  {
+    original: 'https://images.unsplash.com/photo-1519494026892-80bbd2d6fd0d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+  },
+];
+
+const Home = () => {
+  const { ref, inView } = useInView({
+    threshold: 0.5
+  });
+
+  const fade = useAnimation();
+
+  useEffect(() => {
+    if (inView) {
+      fade.start({
+        opacity: 1
+      });
+    }
+
+    if (!inView) {
+      fade.start({
+        opacity: 0
+      });
+    }
+  }, [inView]);
+
+  return (
+    <section className="home__container overflow-hidden" id="home">
+
+      {/* Home Title */}
+      <div className="home__container--title flex justify-center custom:flex-col sm:flex-col md:flex-col lg:flex-row gap-20">
+        <motion.h1
+          initial={{ opacity: 0, marginTop: '50px' }}
+          animate={{ opacity: 1, marginTop: '0px' }}
+          transition={{ type: 'spring', duration: 2, delay: 0.5 }}
+          className="text-5xl whitespace-nowrap custom:text-2xl md:text-4xl ">
+          <strong>Accuracy and Quality<br />
+            <motion.span
+              initial={{ opacity: 0, marginTop: '50px' }}
+              animate={{ opacity: 1, marginTop: '0px' }}
+              transition={{ type: 'spring', duration: 1, delay: 2 }}
+            >
+              Need <span className="text-green-700">not</span> to be <span className="text-green-700">Expensive</span>
+            </motion.span>
+          </strong>
+        </motion.h1>
+        <motion.p
+          initial={{ opacity: 0, marginTop: '50px' }}
+          animate={{ opacity: 1, marginTop: '80px' }}
+          transition={{ type: 'spring', duration: 1, delay: 2 }}
+          className="text-3xl my-20 custom:text-xl md:my-10 mx-5 text-1xl lg:text-2xl my-15 2xl:text-3xl my-15"
+        >
+          JNCE is modern laboratory services Delivering newer flows<br /> by their place website is the first impression.
+        </motion.p>
+      </div>
+
+      <br />
+
+      {/* Image Carousel */}
+      <ImageGallery
+        items={images}
+        autoPlay={true}
+        infinite={true}
+        showThumbnails={false}
+        slideDuration={1000}
+        slideInterval={5000}
+        showFullscreenButton={false}
+        showGalleryFullscreenButton={false}
+        showPlayButton={true}
+        showGalleryPlayButton={true}
+      />
+
+      {/* Basic Info */}
+      <div ref={ref} className="info__container flex items-center justify-center md:gap-5 lg:gap-5 xl:gap-10 2xl:gap-10 flex-row custom:gap-5">
+
+        <motion.div animate={fade} className="card hover:bg-green-800 hover:text-white">
+          <div className="info__container--img absolute top-6">
+            <LazyLoadImage
+              className="w-64"
+              alt="accurate svg"
+              src={accurate}
+            />
+          </div>
+          <div className="info__container--text mt-28 flex flex-col items-center justify-center text-center gap-4">
+            <h1 className="text-2xl text-center lg:text-xl 2xl:text-2xl"><strong>Accurate</strong></h1>
+            <span className="text-xl lg:text-lg 2xl:text-xl">Providing accurate<br /> lab results</span>
+          </div>
+        </motion.div>
+
+        <motion.div animate={fade} transition={{ type: 'fade', delay: 0.25 }} className="card hover:bg-green-800 hover:text-white">
+          <div className="info__container--img absolute top-4 -left-12">
+            <LazyLoadImage
+              className="w-64"
+              alt="cheap svg"
+              src={cheap}
+            />
+          </div>
+          <div className="info__container--text mt-28 flex flex-col items-center justify-center text-center gap-4">
+            <h1 className="text-2xl text-center lg:text-xl 2xl:text-2xl"><strong>Not Expensive</strong></h1>
+            <span className="text-xl lg:text-lg 2xl:text-xl">Accurate but<br /> not expensive</span>
+          </div>
+        </motion.div>
+
+        <motion.div animate={fade} transition={{ type: 'fade', delay: 0.35 }} className="card hover:bg-green-800 hover:text-white">
+          <div className="info__container--img absolute -top-16 -left-36">
+            <LazyLoadImage
+              className="w-64"
+              alt="quality svg"
+              src={quality}
+            />
+          </div>
+          <div className="info__container--text mt-28 flex flex-col items-center justify-center text-center gap-4">
+            <h1 className="text-2xl text-center lg:text-xl 2xl:text-2xl"><strong>Quality</strong></h1>
+            <span className="text-xl lg:text-base 2xl:text-xl">High quality<br /> laboratory equipments</span>
+          </div>
+        </motion.div>
+
+        <motion.div animate={fade} transition={{ type: 'fade', delay: 0.45 }} className="card hover:bg-green-800 hover:text-white">
+          <div className="info__container--img absolute">
+            <LazyLoadImage
+              className="w-52"
+              alt="delivery svg"
+              src={delivery}
+            />
+          </div>
+          <div className="info__container--text mt-28 flex flex-col items-center justify-center text-center gap-4">
+            <h1 className="text-2xl text-center lg:text-xl 2xl:text-2xl"><strong>Prompt Delivery</strong></h1>
+            <span className="text-xl lg:text-base 2xl:text-xl">Satisfy clients<br /> through prompt delivery</span>
+          </div>
+        </motion.div>
+
+      </div>
+
+    </section>
+  )
+}
+
+export default Home;
