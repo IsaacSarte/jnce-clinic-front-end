324431affb4fa4d3b722946a083e3bdcf7cb0abb
Isaac Sarte <aizakku09@gmail.com>
2022-03-25 19:38:52 +0800

    place google map api token on env file

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -76,7 +76,7 @@ const Contact = () => {
           {/* GOOGLE MAP START */}
           <div className="contact__google_map">
             <WrappedMap
-              googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyBCoifrIuftYlU5_KbaZ_-qoqtrjT7Ckws`}
+              googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_MAP_API_TOKEN}`}
               loadingElement={<div style={{ height: `100%` }} />}
               containerElement={<div style={{ height: `400px` }} />}
               mapElement={<div style={{ height: `100%` }} />}
