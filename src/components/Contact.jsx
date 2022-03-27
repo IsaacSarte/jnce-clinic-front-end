e57a8598ce55b3ba0945afa34544add06d5da137
Isaac Sarte <aizakku09@gmail.com>
2022-03-28 01:31:36 +0800

    added user feedback

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // axios
-// import axios from 'axios';
-// import { inquiryURL } from '../api/PublicAPI.jsx';
+import axios from 'axios';
+import { createFeedbackURL } from '../api/UserApi.jsx';
 
 import { GoogleMap, withScriptjs, withGoogleMap, Marker } from 'react-google-maps';
 
 import { CallIcon } from '../subComponents/AllSvg.jsx';
 import { MailIcon } from '../subComponents/AllSvg.jsx';
 import googleMapStyle from '../subComponents/googleMapStyle.jsx';
+
+// Components
+import { SuccessModal } from '../subComponents/SuccessModal.jsx';
 
 const Map = () => {
   return (
@@ -28,20 +31,25 @@ const WrappedMap = withScriptjs(withGoogleMap(Map));
 const Contact = () => {
 
   /* State Management */
-  //const [getInquiries, setGetInquiries] = useState([]);
+  const [getFeedback, setGetFeedback] = useState('');
 
-  // Add Inquiry State
-  // const [fullname, setFullName] = useState('');
-  // const [email, setEmail] = useState('');
-  // const [phone, setPhone] = useState(0);
-  // const [message, setMessage] = useState('');
+  // Add Feedback State
+  const [fullname, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('+63 ');
+  const [message, setMessage] = useState('');
 
   // Status
-  // const [status, setStatus] = useState('Adding Inquiry');
+  const [status, setStatus] = useState('Adding Inquiry');
 
+  // Error Messages
+  const [error, setError] = useState([]);
+  const [fullnameError, setFullNameError] = useState([]);
+  const [emailError, setEmailError] = useState([]);
+  const [messageError, setMessageError] = useState([]);
 
   // useEffect(() => {
-  //   axios.get(inquiryURL)
+  //   axios.get(createFeedbackURL)
   //     .then(res => {
   //       console.log(res.data.data)
   //       setGetInquiries(res.data.data)
@@ -51,17 +59,30 @@ const Contact = () => {
   //     })
   // }, []);
 
-  // const handleAddInquiry = (e) => {
-  //   e.preventDefault();
-  //   const addInquiry = inquiryURL;
+  const handleAddFeedback = async (e) => {
+    e.preventDefault();
+    const addFeedback = createFeedbackURL;
 
-  //   setStatus('Adding Inquiry');
+    setStatus('Adding Feedback');
+    console.log(status);
 
-  //   return axios.post(
-  //     addInquiry,
-  //     { fullname: fullname, email: email, phone: phone, message: message }
-  //   )
-  // }
+    return axios.post(
+      addFeedback,
+      { fullname: fullname, email: email, phone: phone, message: message }
+    )
+      .then((res) => {
+        console.log(res.data.message)
+        setGetFeedback(res.data.message)
+      })
+      .catch((err) => {
+        setError(err.response.data.error);
+        console.log(error);
+        setFullNameError(err.response.data.error.user.fullname)
+        setEmailError(err.response.data.error.user.email)
+        setMessageError(err.response.data.error.feedback.message)
+        console.log(err.response.data.error);
+      })
+  }
 
   return (
     <div className="contact__container" id="contact">
@@ -164,7 +185,7 @@ const Contact = () => {
           <br />
           {/* CONTACT INQUIRY FORM START */}
           <div className="contact__form bg-slate-300 border-t-8 border-green-800 py-4 px-12 flex flex-col">
-            <span className="text-green-800 text-3xl">Online Inquiry form</span><br />
+            <span className="text-green-800 text-3xl">Give us your Thoughts</span><br />
             <span className="font-bold">Feedback from this form is not monitored outside of normal business hours. If you have a medical or mental health emergency, please contact us on our hotlines.</span>
             <span>We are unable to provide medical advice or answer individual questions about treatment - these issues should be raised with your doctor.</span>
             <br />
@@ -172,25 +193,52 @@ const Contact = () => {
               <div className="contact__form--container flex justify-evenly sm:flex-col sm:justify-center custom:flex-col custom:justify-center">
                 <div className="w-1/2 px-2 sm:w-full custom:w-full">
                   <div className="text-gray-700 mb-4 w-full">
-                    <label className="block mb-1 text-green-600 font-bold" for="name">Name</label>
-                    <input className="w-full h-10 px-3 py-6 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline" type="text" id="name" placeholder='full name' required />
+                    <label className="block mb-1 text-green-600 font-bold" for="name">Full Name</label>
+                    <input value={fullname} onChange={(e) => setFullName(e.target.value)} className="w-full h-10 px-3 py-6 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline" type="text" id="name" placeholder='full name' required />
+                    {fullnameError ? (
+                      <span>{fullnameError.map((fullnameErr, index) => (
+                        <li className="list-none text-red-700 font-semibold my-1" key={index}>
+                          <span>{`Fullname ${fullnameErr}`}</span>
+                        </li>
+                      ))}</span>
+                    ) : (
+                      <span></span>
+                    )}
                   </div>
                   <div className="text-gray-700 mb-4 w-full">
                     <label className="block mb-1 text-green-600 font-bold" for="email">Your email</label>
-                    <input className="w-full h-10 px-3 py-6 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline" type="email" id="email" placeholder="valid email" required />
+                    <input value={email} onChange={(e) => setEmail(e.target.value)} className="w-full h-10 px-3 py-6 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline" type="email" id="email" placeholder="valid email" required />
+                    {emailError ? (
+                      <span>{emailError.map((emailErr, index) => (
+                        <li className="list-none text-red-700 font-semibold my-1" key={index}>
+                          <span>{`Email ${emailErr}`}</span>
+                        </li>
+                      ))}</span>
+                    ) : (
+                      <span></span>
+                    )}
                   </div>
                   <div className="text-gray-700 mb-4 w-full">
                     <label className="block mb-1 text-green-600 font-bold" for="phone">Phone</label>
-                    <input className="w-full h-10 px-3 py-6 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline" type="phone" id="phone" placeholder='contact number' required />
+                    <input value={phone} onChange={(e) => setPhone(e.target.value)} className="w-full h-10 px-3 py-6 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline" type="phone" id="phone" placeholder='contact number' required />
                   </div>
                 </div>
                 <div className="w-1/2 px-2 sm:w-full custom:w-full">
                   <div className="text-gray-700 mb-4 w-full">
                     <label className="block mb-1 text-green-600 font-bold" for="message">Message</label>
-                    <textarea id="message" className="w-full h-[240px] px-3 py-2 text-base text-gray-700 placeholder-gray-600 border rounded-lg focus:shadow-outline" placeholder="Leave your concern......" required></textarea>
+                    <textarea value={message} onChange={(e) => setMessage(e.target.value)} id="message" className="w-full h-[240px] px-3 py-2 text-base text-gray-700 placeholder-gray-600 border rounded-lg focus:shadow-outline" placeholder="Leave your concern......" required></textarea>
+                    {messageError ? (
+                      <span>{messageError.map((messageErr, index) => (
+                        <li className="list-none text-red-700 font-semibold my-1" key={index}>
+                          <span>{`Message ${messageErr}`}</span>
+                        </li>
+                      ))}</span>
+                    ) : (
+                      <span></span>
+                    )}
                   </div>
                   <div className="w-full">
-                    <button type="submit" className="relative w-full sm:w-full custom:w-full inline-flex items-center justify-start py-3 pl-4 pr-12 overflow-hidden font-semibold text-indigo-600 transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-gray-50 group">
+                    <button onClick={(e) => handleAddFeedback(e)} type="submit" className="relative w-full sm:w-full custom:w-full inline-flex items-center justify-start py-3 pl-4 pr-12 overflow-hidden font-semibold text-indigo-600 transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-gray-50 group">
                       <span className="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-indigo-600 group-hover:h-full"></span>
                       <span className="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12">
                         <svg className="w-5 h-5 text-green-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
@@ -200,6 +248,18 @@ const Contact = () => {
                       </span>
                       <span className="relative w-full text-center transition-colors duration-200 ease-in-out group-hover:text-white">Submit your Inquiry</span>
                     </button>
+                    <br /><br />
+                    {getFeedback ? (
+                      <SuccessModal
+                        getFeedback={getFeedback}
+                        setFullName={setFullName}
+                        setEmail={setEmail}
+                        setPhone={setPhone}
+                        setMessage={setMessage}
+                      />
+                    ) : (
+                      <></>
+                    )}
                   </div>
                 </div>
               </div>
