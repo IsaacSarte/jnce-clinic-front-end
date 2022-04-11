ac10f12b013f3d2964a570eac0dc0f9be93c4069
Isaac Sarte <aizakku09@gmail.com>
2022-04-11 12:43:46 +0800

    added images on services

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -9,11 +9,13 @@ import { useInView } from 'react-intersection-observer';
 import { motion, useAnimation } from 'framer-motion';
 
 const Services = () => {
+
   const { ref, inView } = useInView({
     threshold: 0.2
   });
 
   const animationLeft = useAnimation();
+  const fade = useAnimation();
 
   useEffect(() => {
     if (inView) {
@@ -24,6 +26,9 @@ const Services = () => {
           type: 'spring', duration: 1, bounce: 0.3
         }
       });
+      fade.start({
+        opacity: 1
+      })
     }
 
     if (!inView) {
@@ -31,6 +36,9 @@ const Services = () => {
         x: '-100px',
         opacity: 0
       });
+      fade.start({
+        opacity: 0
+      })
     }
   })
 
@@ -46,35 +54,74 @@ const Services = () => {
 
         <br /><br />
 
-        <div className="services__container--section--img bg-green-800 w-full h-auto">
-          <div className="flex items-center justify-center gap-20 mb-10">
+        <div className="services__container--section--img bg-green-800 max-w-[90vw] w-full h-auto custom:h-[10px] -mt-2">
+          <div ref={ref} className="services__container--section--images max-w-2xl grid grid-cols-3 justify-items-center p-8 gap-4 custom:hidden -mt-4">
 
-            <div className="">
-              <div className="service__container--img h-40 text-white">
-                Image 1
-              </div>
-              <div className="text-white">
-                Image 2
-              </div>
-            </div>
+            <motion.div
+              animate={fade}
+              transition={{ type: 'fade', duration: 0.25, delay: 0 }}
+            >
+              <LazyLoadImage
+                className="h-[145px]"
+                alt="Service 1"
+                src={'https://images.unsplash.com/photo-1614308456595-a59d48697ea8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=200'}
+              />
+            </motion.div>
 
-            <div className="">
-              <div className="service__container--img h-48 mt-8 text-white">
-                Image 3
-              </div>
-              <div className="text-white">
-                Image 4
-              </div>
-            </div>
+            <motion.div
+              animate={fade}
+              transition={{ type: 'fade', duration: 0.25, delay: 0.05 }}
+            >
+              <LazyLoadImage
+                className="h-[175px]"
+                alt="Service 2"
+                src={'https://images.unsplash.com/photo-1628348070889-cb656235b4eb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8ZWNnfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=200&q=200'}
+              />
+            </motion.div>
 
-            <div className="">
-              <div className="service__container--img h-56 mt-16 text-white">
-                Image 5
-              </div>
-              <div className="text-white">
-                Image 6
-              </div>
-            </div>
+            <motion.div
+              animate={fade}
+              transition={{ type: 'fade', duration: 0.25, delay: 0.1 }}
+            >
+              <LazyLoadImage
+                className="h-[205px]"
+                alt="Service 3"
+                src={'https://images.unsplash.com/photo-1532938911079-1b06ac7ceec7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=200'}
+              />
+            </motion.div>
+
+            <motion.div
+              animate={fade}
+              transition={{ type: 'fade', duration: 0.25, delay: 0.15 }}
+            >
+              <LazyLoadImage
+                className="h-[205px] -mt-16"
+                alt="Service 4"
+                src={'https://images.unsplash.com/photo-1630531210843-d6f343ad1f90?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8dWx0cmFzb3VuZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=200&q=200'}
+              />
+            </motion.div>
+
+            <motion.div
+              animate={fade}
+              transition={{ type: 'fade', duration: 0.25, delay: 0.2 }}
+            >
+              <LazyLoadImage
+                className="h-[175px] -mt-8"
+                alt="Service 5"
+                src={'https://images.unsplash.com/photo-1626736985932-c0df2ae07a2e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=200'}
+              />
+            </motion.div>
+
+            <motion.div
+              animate={fade}
+              transition={{ type: 'fade', duration: 0.25, delay: 0.25 }}
+            >
+              <LazyLoadImage
+                className="h-[150px] -mt-[0.4rem]"
+                alt="Service 6"
+                src={'https://images.unsplash.com/photo-1605549188315-8aa899c3d35f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGNvdmlkfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=200&q=200'}
+              />
+            </motion.div>
 
           </div>
         </div>
